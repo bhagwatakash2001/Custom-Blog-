35b90fc563cf4e03a8866f2e4c6a956230aa7e64
Akash Bhagwat <70906580+bhagwatakash2001@users.noreply.github.com>
2020-10-07 18:21:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-        var menu_btn = document.getElementById('menu-btn');
-        var side_nav = document.getElementById('side-nav');
+let btn = document.getElementById('cta-btn');
+let overlay = document.getElementById('overlay');
 
-        side_nav.style.right = "-250px";
-
-        menu_btn.onclick = function (){
-            if(side_nav.style.right == "-250px"){
-
-            side_nav.style.right = "0";
-
-            }   else{
-
-                side_nav.style.right = "-250px";
-            }
-        }
-        
+btn.addEventListener('click', () => {
+    overlay.style.display = 'grid';
+    overlay.classList.add('animate-overlay')
+})
